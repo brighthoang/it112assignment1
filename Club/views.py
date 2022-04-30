4156dc011bb8f0e01156c040604d8ecda8f71f87
brighthoang <brihoa4002@gmail.com>
2022-04-30 16:15:27 -0700

    second resources view

--- Club/views.py
+++ Club/views.py
@@ -1,6 +1,12 @@
+import re
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 
-def index(r):
-    return render(r, 'Club/index.html')
+def index(request):
+    return render(request, 'Club/index.html')
+
+def resource(request):
+    resource_list = Resource.objects.all()
+    return render(request, 'Club/resources.html', {'resource_list': resource_list})
